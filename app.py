604f00656296c12248a2466c93efbea5eea5b8c3
Adityasaica <143105139+Adityasaica@users.noreply.github.com>
2024-03-19 10:57:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+print("Aditya Sai")
+print("With shortcut contro+r")
+#pic of the dog
+print('o----')
+print(' ||||')
+#python compiles executes from top to bottom line by line
+#multipling a string will be give a string of a snippet repeated with n times
+
+print('*' *10)
+#variables
+price=10
+#a space in the memory that stores the data
+print(price)
+#10 was stored or converted to binary
+price=20
+print(price)
+#floating point number
+
+rating=4.9
+name='adithya'
+boolean=True
+boolean=False
+#practice for variables
+patient_name='JOhn Smith'
+Patient_age=20
+patient_new=True
+
+#input
+#we take input by input() function
+
+name=input()
+print("hello",name)
+
+print("Hello "+name)
+
+name1=input("Enter the name of you? ")
+print("Good! to see you "+name1)
+
+
+#practice assesment-2
+
+name_p=input("Enter the name of the person:")
+col=input("Enter favourite color:")
+print(name_p+' '+'likes'+' '+col)
+
+#type conversion
+
+birthyear=input("Enter the year of the birth:")
+#thae above line takes the string as a input
+#convert it to the integer
+birthyear=int(birthyear)
+print(2024-birthyear)
+
+#strings
+
+
+course='python for beginner'
+course='pyton\'course for the begginer\'s'
+print(course)
+
+newone="""
+hi john here is your first email thanks for your support
+
+
+"""
+print(course[0])
+print(course[-1])
---
+++ strings.py
@@ -0,0 +1,12 @@
+course='python for beginner'
+course='pyton\'course for the begginer\'s'
+print(course)
+
+newone="""
+hi john here is your first email thanks for your support
+
+
+"""
+print(course[0])
+print(course[-1])
+print(course[0:3])
